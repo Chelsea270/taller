180a57ae5a2f15b8824ae8633a73cd54c6427640
Chelsea270 <cyquintanilla@uamv.edu.ni>
2026-04-15 20:33:21 -0600

    Taller

---
+++ src/main/java/taller/Dueño.java
@@ -0,0 +1,12 @@
+package taller;
+
+import java.util.List;
+
+public class Dueño {
+    private int id;
+    private String nombre;
+    private String mascota;
+    private List<Mascotas> mascotas;
+
+    public Dueño ( int id, St)
+}
