191f490d814dd1c4c770381c894d5ce0990c2dcd
Chelsea270 <cyquintanilla@uamv.edu.ni>
2026-04-16 21:20:23 -0600

    modificaciones del codigo.

---
+++ src/main/java/TallerAbstraccion/ClinicaVeterinaria.java
@@ -0,0 +1,47 @@
+package TallerAbstraccion;
+
+import java.util.Scanner;
+
+public class ClinicaVeterinaria {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Dueno dueno1 = new Dueno("Carlos Pérez", "8888-1234", "Managua");
+        Mascota mascota1 = new Mascota("Firulais", "Perro", 5);
+
+        System.out.println( " -- BIENVENIDO A LA CLINICA VETERINARIA ' Los Arcos ' ");
+
+        Cita cita1 = new Cita("20/04/2026", "10:00 AM", "Vacunación", "Dr. López");
+
+        System.out.println("Cita programada a nombre de" + mascota1.getNombre()) + "el dia: "+ cita1.getFecha());
+
+        System.out.println(" ¿Desea modificar la cita? ");
+        String respuesta = sc.nextLine();
+
+        if (respuesta.equalsIgnoreCase("si")) {
+            System.out.println("Inserte el nombre de la nueva fecha: ");
+            String nuevaFecha = sc.nextLine();
+            System.out.println("Inserte la hora deseada: ");
+            String nuevaHora = sc.nextLine();
+
+            cita1.modificarCita(nuevaHora, nuevaFecha);
+
+            System.out.println(" ¡Actualizacion exitosa de los datos! fecha de la cita: "+ nuevaFecha + " a las: "+ nuevaHora);
+
+        }
+        else {
+
+            System.out.println("¡Cita guardada exitosamente!");
+
+        }
+
+        sc.close();
+
+        dueno1.registrarMascota(mascota1);
+        mascota1.agregarCita(cita1);
+        cita1.registrarDiagnostico("Mascota saludable, vacuna aplicada.");
+
+
+    }
+}
+
